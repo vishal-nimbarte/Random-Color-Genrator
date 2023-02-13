0ab79e4c2c9bc161de667fd85ffb266d9d909e28
Vishal Nimbarte <79621720+vishal-nimbarte@users.noreply.github.com>
2023-02-13 16:41:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const containerE1 = document.querySelector(".container");
+
+for (let index = 0; index < 30; index++) 
+{
+    const colorContainerE1 = document.createElement("div");
+    colorContainerE1.classList.add("color-container");
+    containerE1.appendChild(colorContainerE1);   
+}
+
+const colorContainerE1s = document.querySelectorAll(".color-container");
+
+generateColors()
+
+function generateColors(){
+    colorContainerE1s.forEach(
+    (colorContainerE1)=>{
+        const newColorCode = randomColor();
+        colorContainerE1.style.backgroundColor = "#"+newColorCode;
+        colorContainerE1.innerText = "#"+newColorCode;
+    });
+}
+
+function randomColor()
+{
+    const chars = "0123456789abcdef";
+    const colorCodelength = 6;
+    let colorCode = "";
+    for(let index = 0;index<colorCodelength;index++)
+    {
+        const randomNum = Math.floor(Math.random()*chars.length);
+        // console.log(randomNum);
+        colorCode += chars.substring(randomNum,randomNum+1);
+        // console.log(colorCode,randomNum);
+        
+    }
+    return colorCode;
+}
